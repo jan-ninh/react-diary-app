67d70b34a3738c13c206249e45105c49ddd293de
Jan <janninh@gmx.de>
2025-11-27 18:40:27 +0100

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Destinations from "./pages/Destinations";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Destinations />
+      <About />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <button className="btn btn-primary"> Home </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
